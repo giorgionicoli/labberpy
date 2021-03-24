266f45283a83da79f78af7315f7777ca92c60fbc
Giorgio Nicoli’ <75094347+qJakc@users.noreply.github.com>
2021-03-24 19:41:14 +0100

    "Labeler and Label classes review"
    Changed the interface of the Label classes to be more flexible. The Labeler uses this new set of input args for Label

--- labdata.py
+++ labdata.py
@@ -1,5 +1,6 @@
 from typing import List
 import json
+import regex as re
 
 from labber import LabberFile
 from labvar import LabVar
@@ -10,7 +11,7 @@ class DataExtractor:
     def __init__(self, file_name: str):
         self._labber_file = LabberFile(file_name)
 
-    def get_vars(self, var_list: List):
+    def get_vars(self, *var_list: List):
         data_out = list()
         for var_name in var_list:
             data = self._labber_file.get_phys_data(var_name)
@@ -23,62 +24,77 @@ class Labeler:
     _label_templates = {}
 
     def __init__(self, json_path: str = "label_templates.json"):
-        with open(json_path) as json_file:
-            self._label_templates.update(json.load(json_file))
+        try:
+            with open(json_path) as json_file:
+                self._label_templates.update(json.load(json_file))
+        except Exception:
+            print(f"Labeler.__init__() Exception: Json file not found."
+                  f"Continuing without updating _label_templates.")
 
     def _autolabel(self, var_name, unit):
-        template_info = self._label_templates.get(unit, None)
-        if template_info is not None:
-            label = self.make_label(**template_info)
-        return label
+        label_info = self._label_templates.get(unit, None)
+        if label_info is not None:
+            var_parse = re.match(label_info["symbol"] + r'_?([a-zA-Z0-9]+)', var_name)
+            label_info.update(self._label_templates.get(var_parse.group(1).title(), {}))
+            try:
+                return Label(**label_info)
+            except Exception:
+                print(f"Labeler._autolabel() Exception: args passed to Label() object not correct."
+                      f"Continuing with empty Label() object for variable --> {var_name}")
+        return Label()
 
-    def make_label(
+    '''def make_label(
         self,
         name: str = "",
         symbol: str = "",
         unit: str = "",
-        subscript: str = "",
         prefix: str = "",
-        pre_unit: str = "",
+        subscript: str = "",
+        superscript: str = "",
+        multiplier: str = "",
         short_form: bool = True,
     ):
-        name = fr"{prefix} {name}"
-        symbol = fr"${symbol}_\mathrm{subscript}$"
-        unit = fr"{unit}"
-        return Label(name, symbol, unit)
+
+        return Label(
+            name = name,
+            symbol = symbol,
+            unit = unit,
+            prefix = prefix,
+            subscript = subscript,
+            superscript = superscript,
+            multiplier = multiplier
+        )
+
+    def _parse_var_name(self, var_name: str, var_symbol: str) -> str:
+        var_parse = re.match(var_symbol + r'_?([a-zA-Z0-9]+)', var_name)
 
 
-'''
-def _guessSymbol(var_name: str) -> str:
-    """uses regex and common nomenclature of labber variables to create
-        LaTeX compatible str variable symbols"""
+        no_match = True
 
-    no_match = True
-
-    v_test = re.match(r'V_?([a-zA-Z0-9]+)', var_name)
-    if v_test:
-        var_symbol = r'$V_\mathrm{' + v_test.group(1) + r'}$'
-        no_match = False
-
-    if no_match:
-        b_test = re.match(r'([mM]agnetic|[bB])_?[fF]ield', var_name)
-        if b_test:
-            var_symbol = r'$B$'
+        v_test = re.match(r'V_?([a-zA-Z0-9]+)', var_name)
+        if v_test:
+            var_symbol = r'$V_\mathrm{' + v_test.group(1) + r'}$'
             no_match = False
 
-    if no_match:
-        i_test = re.match(r'I_?([a-zA-Z0-9]+)', var_name)
-        if i_test:
-            var_symbol = r'$I_\mathrm{' + i_test.group(1) + r'}$'
-            no_match = False
+        if no_match:
+            b_test = re.match(r'([mM]agnetic|[bB])_?[fF]ield', var_name)
+            if b_test:
+                var_symbol = r'$B$'
+                no_match = False
 
-    if no_match:
-        var_symbol = ''
-    else:
-        phase_test = re.search(r'[pP]hase', var_name)
-        if phase_test:
-            # add the \angle character in the front
-            var_symbol = r'$\angle ' + var_symbol[1:]
+        if no_match:
+            i_test = re.match(r'I_?([a-zA-Z0-9]+)', var_name)
+            if i_test:
+                var_symbol = r'$I_\mathrm{' + i_test.group(1) + r'}$'
+                no_match = False
 
-    return var_symbol
-    '''
+        if no_match:
+            var_symbol = ''
+        else:
+            phase_test = re.search(r'[pP]hase', var_name)
+            if phase_test:
+                # add the \angle character in the front
+                var_symbol = r'$\angle ' + var_symbol[1:]
+
+        return var_symbol'''
+
--- labels.py
+++ labels.py
@@ -4,17 +4,42 @@ class Label:
         name: str = "",
         symbol: str = "",
         unit: str = "",
+        prefix: str = "",
+        subscript: str = "",
+        superscript: str = "",
+        multiplier: str = "",
     ):
         self.name = name
         self.symbol = symbol
         self.unit = unit
+        self.prefix = prefix
+        self.subscript = subscript
+        self.superscript = superscript
+        self.multiplier = multiplier
 
     @property
     def label(self):
-        return fr"{self.name} {self.symbol} ({self.unit})"
+        name = self.name
+        if self.prefix:
+            name = self.prefix + " " + name
+
+        symbol = fr"${self.symbol}"
+        if self.subscript:
+            symbol += fr"_\mathrm{{{self.subscript}}}"
+        if self.superscript:
+            symbol += fr"^\mathrm{{{self.superscript}}}"
+        symbol += "$"
+
+        unit = self.unit
+        if self.multiplier:
+            unit = self.multiplier + unit
+
+        return fr"{name} {symbol} ({unit})"
 
     def __repr__(self):
-        return fr"Label('{self.name}', '{self.symbol}', '{self.unit}')"
+        obj_repr = [f"{k}='{v}'" for k, v in vars(self).items()]
+        obj_repr = "Label(" + ", ".join(obj_repr) + ")"
+        return obj_repr
 
     def __str__(self):
         return self.label
